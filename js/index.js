56f05fae5ae3d7590c459079b36919b3e95d30a5
jonathan potter <jonathan.potter.business@gmail.com>
2019-08-19 21:09:48 -0700

    add the cubes

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ uniform sampler2D iChannel0;
 // By Daedelus: https://www.shadertoy.com/user/Daedelus
 // license: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 #define TIMESCALE 0.25
-#define TILES 18.0
+#define TILES 8.0
 #define COLOR 0.7, 1.6, 2.8
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord ){
@@ -29,10 +29,21 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ){
 	fragColor = vec4(COLOR, 1.0) * p;
 }
 
+varying vec2 vUv;
+
 void main() {
-    mainImage(gl_FragColor, gl_FragCoord.xy);
+    mainImage(gl_FragColor, vUv * iResolution.xy);
   }
 `
+
+const vertexShader = `
+  varying vec2 vUv;
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  }
+`
+
 const loader = new THREE.TextureLoader()
 const texture = loader.load('assets/noise.png')
 texture.minFilter = THREE.NearestFilter
@@ -41,33 +52,61 @@ texture.wrapS = THREE.RepeatWrapping
 texture.wrapT = THREE.RepeatWrapping
 const uniforms = {
     iTime: { value: 0 },
-    iResolution:  { value: new THREE.Vector3() },
+    iResolution:  { value: new THREE.Vector3(1, 1, 1) },
     iChannel0: { value: texture },
 }
 
 function main () {
     const canvas = document.querySelector('#canvas')
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
     const renderer = new THREE.WebGLRenderer({canvas})
     renderer.autoClearColor = false
 
-    /* eslint-disable indent */
-    const camera = new THREE.OrthographicCamera(
-        -1, // left
-         1, // right
-         1, // top
-        -1, // bottom
-        -1, // near,
-         1, // far
-    )
-    /* eslint-enable indent */
+    const fov = 75
+    const aspect = 2  // the canvas default
+    const near = 0.1
+    const far = 5
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+    camera.position.z = 2
 
     const scene = new THREE.Scene()
-    const plane = new THREE.PlaneBufferGeometry(2, 2)
+
+
+    {
+        const color = 0xFFFFFF
+        const intensity = 1
+        const light = new THREE.DirectionalLight(color, intensity)
+        light.position.set(-1, 2, 4)
+        scene.add(light)
+    }
+
+    const boxWidth = 1
+    const boxHeight = 1
+    const boxDepth = 1
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
+
     const material = new THREE.ShaderMaterial({
+        vertexShader,
         fragmentShader,
         uniforms,
     })
-    scene.add(new THREE.Mesh(plane, material))
+
+    function makeInstance (geometry, color, x) {
+        const cube = new THREE.Mesh(geometry, material)
+        scene.add(cube)
+
+        cube.position.x = x
+
+        return cube
+    }
+
+    const cubes = [
+        makeInstance(geometry, 0x44aa88,  0),
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0xaa8844,  2),
+    ]
+
 
     function resizeRendererToDisplaySize (renderer) {
         const canvas = renderer.domElement
@@ -85,9 +124,14 @@ function main () {
 
         resizeRendererToDisplaySize(renderer)
 
-        const canvas = renderer.domElement
-        uniforms.iResolution.value.set(canvas.width, canvas.height, 1)
         uniforms.iTime.value = time
+
+        cubes.forEach((cube, ndx) => {
+            const speed = 1 + ndx * .1
+            const rot = time * speed
+            cube.rotation.x = rot
+            cube.rotation.y = rot
+        })
 
         renderer.render(scene, camera)
 
